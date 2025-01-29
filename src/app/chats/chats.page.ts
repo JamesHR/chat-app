0e7f250d1a4d9bc8189db851b191d9401839834b
JamesHR <jaimehr2101@gmail.com>
2025-01-29 16:25:28 -0600

    feat: generar array de chats
    Agregar propiedad chats como array para concentrar todos los chat de la aplicación

--- src/app/chats/chats.page.ts
+++ src/app/chats/chats.page.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatsPage implements OnInit {
 
+  chats: any[] = [
+    {
+      username: 'Jaime HR',
+      message: 'Bienvenido a la práctica chatApp',
+      date: '14:25',
+      avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
